088bb522c04a730339ce9718ac653d19fa191440
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-27 09:14:44 -0800

    update build scripts

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("e924ef7")]
-[assembly: AssemblySemanticVersion("1.0.5-test+e924ef7")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+e924ef7, Revision=1.0.5.1199817386")]
+[assembly: AssemblyCommit("0f3791c")]
+[assembly: AssemblySemanticVersion("1.0.5-test+0f3791c")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+0f3791c, Revision=1.0.5.1077738052")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
