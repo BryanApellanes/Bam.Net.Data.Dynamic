c312dfec7254875239ced29064dfbd04e61a3bc2
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-27 07:05:56 -0800

    pre submodule commit: update

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("cfe1f60")]
-[assembly: AssemblySemanticVersion("1.0.5-test+cfe1f60")]
-[assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+cfe1f60, Revision=1.0.5.222076960")]
+[assembly: AssemblyCommit("e924ef7")]
+[assembly: AssemblySemanticVersion("1.0.5-test+e924ef7")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+e924ef7, Revision=1.0.5.1199817386")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
