5d98bb408ed17c7bedef15d3227cc2dc76a9fcc2
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-28 09:36:25 -0800

    updated build scripts: proper handle windows overrides

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("0f3791c")]
-[assembly: AssemblySemanticVersion("1.0.5-test+0f3791c")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+0f3791c, Revision=1.0.5.1077738052")]
+[assembly: AssemblyCommit("088bb52")]
+[assembly: AssemblySemanticVersion("1.0.5-test+088bb52")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+088bb52, Revision=1.0.5.3863489087")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
