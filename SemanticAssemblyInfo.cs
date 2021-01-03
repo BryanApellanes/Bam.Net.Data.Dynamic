eeb2153d3485a24acd892a31269c1fb2c88d24f5
Bryan Apellanes <bryan.apellanes@gmail.com>
2021-01-03 11:09:24 -0800

    refactor build scripts

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("5d98bb4")]
-[assembly: AssemblySemanticVersion("1.0.5-test+5d98bb4")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+5d98bb4, Revision=1.0.5.925671927")]
+[assembly: AssemblyCommit("a87aea8")]
+[assembly: AssemblySemanticVersion("1.0.5-test+a87aea8")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+a87aea8, Revision=1.0.5.3909952552")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
