f16f9fe71b155ecc699773dedcec36f99f5ac2b0
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-10 08:55:23 -0700

    wip: reference nugets instead of project

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyCommit("39321ee")]
-[assembly: AssemblySemanticVersion("1.0.6-test+39321ee")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.6-test+39321ee, Revision=1.0.6.3578340154")]
+[assembly: AssemblyCommit("9f87ce9")]
+[assembly: AssemblySemanticVersion("1.0.6-test+9f87ce9")]
+[assembly: AssemblyDescription("Bam Framework for .NET Core; SemanticVersion=1.0.6-test+9f87ce9, Revision=1.0.6.3427799092")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
