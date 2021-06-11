c180f3dadce2e297b6984b8a43c7e0030d694d1e
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-11 10:10:28 -0700

    update build scripts for better package handling

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyCommit("9f87ce9")]
-[assembly: AssemblySemanticVersion("1.0.6-test+9f87ce9")]
-[assembly: AssemblyDescription("Bam Framework for .NET Core; SemanticVersion=1.0.6-test+9f87ce9, Revision=1.0.6.3427799092")]
+[assembly: AssemblyCommit("f16f9fe")]
+[assembly: AssemblySemanticVersion("1.0.6-f16f9fe")]
+[assembly: AssemblyDescription("Components for dynamic data class generation for the Bam Framework; SemanticVersion=1.0.6-f16f9fe, Revision=1.0.6.1665731778")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
