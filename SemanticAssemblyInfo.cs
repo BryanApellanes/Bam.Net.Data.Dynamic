ceda3530e71792be6ddff5505112c819c7b35ce5
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-11 20:34:00 -0700

    update builds

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyCommit("f16f9fe")]
-[assembly: AssemblySemanticVersion("1.0.6-f16f9fe")]
-[assembly: AssemblyDescription("Components for dynamic data class generation for the Bam Framework; SemanticVersion=1.0.6-f16f9fe, Revision=1.0.6.1665731778")]
+[assembly: AssemblyCommit("c180f3d")]
+[assembly: AssemblySemanticVersion("1.0.6-c180f3d")]
+[assembly: AssemblyDescription("Components for dynamic data class generation for the Bam Framework; SemanticVersion=1.0.6-c180f3d, Revision=1.0.6.593959982")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
