c25095239d90cc79a94831a98e30f3c1e7c2e4c4
Bryan Apellanes <bryan@threeheadz.com>
2021-12-04 10:51:52 -0800

    update version

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -1,9 +1,8 @@
 using System.Reflection;
 using Bam.Net;
 
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: AssemblyCommit("ceda353")]
-[assembly: AssemblySemanticVersion("1.0.6-dev+ceda353")]
-[assembly: AssemblyDescription("Components for dynamic data class generation for the Bam Framework; SemanticVersion=1.0.6-dev+ceda353, Revision=1.0.6.434393664")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblySemanticVersion("2.0.0")]
+[assembly: AssemblyDescription("Components for dynamic data class generation for the Bam Framework")]
 [assembly: AssemblyProduct("bam.net.data.dynamic")]
